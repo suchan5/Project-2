9695a667e0965364104ab3a9546f7803cdd1f76f
Su Chan <suchan5@hotmail.com>
2020-05-10 15:06:11 +0800

    clicking search button enabled
    user can type in the character name (e.g. Harry Potter) which they want to searh for in the search bar and click the search button. Information is retrieved from data (name, date of birth, house, ancestry, patronus, wand)

--- script.js
+++ script.js
@@ -10,6 +10,11 @@
 
 
 
+    $ ('#charSearch').click(function(e){
+      e.preventDefault();
+      
+      let userInput = $("#charName").val();
+
       $.ajax({
         method: "GET",
         url: "http://hp-api.herokuapp.com/api/characters",        
@@ -17,10 +22,27 @@
         .done(function( msg ) {
           console.log( msg );
 
-
-          
+          for (let n of msg) {
+            if (userInput.toLowerCase() === (n.name).toLowerCase ()) {
+              $( ".card-title" ).append( `<strong>${n.name}</strong>` );
+              $( ".card-img" ).append( `<img src="${n.image}"/>` );
+              $( "#house" ).append( `${n.house}` );
+              $( "#wood" ).append( `${n.wand.wood}` );
+              $( "#core" ).append( `${n.wand.core}` );
+              $( "#ancestry" ).append( `${n.ancestry}` );
+              $( "#patronus" ).append( `${n.patronus}` );
+              $( "#dob" ).append( `${n.dateOfBirth}` ); 
+            }
+            
+          }
+           
 
 
 
         });
+
+    });
+
+
+      
 
