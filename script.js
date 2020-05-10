108021b1bf244bd4ce977df117cf52d9488d8eea
Su Chan <suchan5@hotmail.com>
2020-05-10 21:31:42 +0800

    search results to be hidden
    search results are only shown when user searches. Also search results are enlarged to a bigger view when user hover the mouse on the table which shows the search results

--- script.js
+++ script.js
@@ -10,38 +10,52 @@
 
 
 
-    $ ('#charSearch').click(function(e){
-      e.preventDefault();
-      
-      let userInput = $("#charName").val();
 
-      $.ajax({
-        method: "GET",
-        url: "http://hp-api.herokuapp.com/api/characters",        
-      })
-        .done(function( msg ) {
-          console.log( msg );
+    // when user search their favourite character
+    $(document).ready(function () {
+      $(".card").hide();
 
-          for (let n of msg) {
-            if (userInput.toLowerCase() === (n.name).toLowerCase ()) {
-              $( ".card-title" ).append( `<strong>${n.name}</strong>` );
-              $( ".card-img" ).append( `<img src="${n.image}"/>` );
-              $( "#house" ).append( `${n.house}` );
-              $( "#wood" ).append( `${n.wand.wood}` );
-              $( "#core" ).append( `${n.wand.core}` );
-              $( "#ancestry" ).append( `${n.ancestry}` );
-              $( "#patronus" ).append( `${n.patronus}` );
-              $( "#dob" ).append( `${n.dateOfBirth}` ); 
+
+      $ ('#charSearch').click(function(e){
+        e.preventDefault();
+  
+        $(".card").show();
+
+        let userInput = $("#charName").val();
+  
+        $.ajax({
+          method: "GET",
+          url: "http://hp-api.herokuapp.com/api/characters",        
+        })
+          .done(function( msg ) {
+            console.log( msg );
+  
+            for (let n of msg) {
+              if (userInput.toLowerCase() === (n.name).toLowerCase ()) {
+                $( ".card-title" ).append( `<strong>${n.name}</strong>` );
+                $( ".card-img" ).append( `<img src="${n.image}"/>` );
+                $( "#house" ).append( `${n.house}` );
+                $( "#wood" ).append( `${n.wand.wood}` );
+                $( "#core" ).append( `${n.wand.core}` );
+                $( "#ancestry" ).append( `${n.ancestry}` );
+                $( "#patronus" ).append( `${n.patronus}` );
+                $( "#dob" ).append( `${n.dateOfBirth}` ); 
+              }
+                
             }
-            
-          }
-           
+              
+  
+  
+  
+          });
+  
+      });
 
 
 
-        });
+    })
 
-    });
+      
 
 
       
