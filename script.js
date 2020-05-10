234e062cfd35527dc2a3b1dbc6e1d3470157f0e2
Su Chan <suchan5@hotmail.com>
2020-05-10 14:10:12 +0800

    API tested with jQuery Ajax

--- script.js
+++ script.js
@@ -6,3 +6,21 @@
       function closeNav() {
         document.getElementById("myNav").style.height = "0%";
       };
+
+
+
+
+      $.ajax({
+        method: "GET",
+        url: "http://hp-api.herokuapp.com/api/characters",        
+      })
+        .done(function( msg ) {
+          console.log( msg );
+
+
+          
+
+
+
+        });
+
