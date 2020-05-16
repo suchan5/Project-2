de1ce38b79917dc5067e163994187c56a5dd1536
Su Chan <suchan5@hotmail.com>
2020-05-16 21:28:27 +0800

    button is active on click, button is disabled when game is playing

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// when page loaded
 $(function () {
     $(document).ready(function () {
         console.log('ready!');
@@ -9,19 +10,101 @@ $(function () {
 
 
 
+// when user clicks button : active on click
+(function() {
+    $('.btn').click(function () {
+        $('.btn').removeClass('active').addClass('inactive');
+        $(this).removeClass('inactive').addClass('active');
+    });
+})();
 
 
+// when user clicks Easy-mode button
+(function () {
+    const easyMode = document.querySelector('#easyMode');
+
+    function clickEasyModeHandler(e) {
+        $('.stageEasy').show();
+    };
+
+    easyMode.addEventListener('click', clickEasyModeHandler);
+
+
+    // Hard-mode is disabled while user plays Easy-mode
+    (function () {
+        const ifClick = document.querySelector('#easyMode');
+
+        function clickHandler(e) {
+            const disabled = document.querySelector('#hardMode');
+            disabled.disabled = true;
+        };
+
+        ifClick.addEventListener('click', clickHandler);
+    })();
+
+
+})();
+
+
+
+// when Easy-mode is playing
 (function () {
 
-    const stage = document.querySelector('.stage');
+    const stageEasy = document.querySelector('.stageEasy');
 
-    function Play(e) {
+    function EasyModePlay(e) {
         console.log(e.target);
 
         if (e.target.classList.contains('dementor')) {
-            stage.removeChild(e.target);
+            stageEasy.removeChild(e.target);
         };
     };
 
-    stage.addEventListener('click', Play);
+    stageEasy.addEventListener('click', EasyModePlay);
+})();
+
+
+
+// when user clicks Hard-mode button
+(function () {
+    const easyMode = document.querySelector('#hardMode');
+
+    function clickHardModeHandler(e) {
+        $('.stageHard').show();
+    };
+    
+    hardMode.addEventListener('click', clickHardModeHandler);
+
+
+
+    // Easy-mode is disabled while user plays Hard-mode
+    (function () {
+        const ifClick = document.querySelector('#hardMode');
+
+        function clickHandler(e) {
+            const disabled = document.querySelector('#easyMode');
+            disabled.disabled = true;
+        };
+
+        ifClick.addEventListener('click', clickHandler);
+    })();
+})();
+
+
+
+
+// When Hard-mode is playing
+(function () {
+
+    const stageHard = document.querySelector('.stageHard');
+
+    function hardModePlay(e) {
+        console.log(e.target);
+
+        if (e.target.classList.contains('dementor')) {
+            stageHard.removeChild(e.target);
+        };
+    };
+
+    stageHard.addEventListener('click', hardModePlay);
 })();
