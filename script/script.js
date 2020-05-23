ee4515de515c817188246ee3b49f8d7e5a13c9cc
Su Chan <suchan5@hotmail.com>
2020-05-23 23:53:51 +0800

    navbar id names amended for better reference and button added for user to scroll back to the top

--- script/script.js
+++ script/script.js
@@ -23,6 +23,36 @@
 
 
 
+    //
+    (function () {        
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                mybutton.style.display = "block";
+            } else if (document.body.scrollTop < 200 || document.documentElement.scrollTop < 200) {
+                mybutton.style.display = "none";
+            } else {
+                mybutton.style.opacity = "none";
+            }
+        }
+
+        const mybutton = document.getElementById("scroll_btn");
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+
+        mybutton.addEventListener ('click', topFunction);
+    })();
+    
+
+
+
+
     // when document loaded
     $(document).ready(function () {
       $( ".h1" ).hide().fadeIn(3000);   
